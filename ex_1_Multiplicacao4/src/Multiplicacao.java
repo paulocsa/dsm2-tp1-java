b21e95243eaaf2e1546fb8258c7f026f802b50ac
paulocsa <paulocandiani04@gmail.com>
2024-03-27 20:01:47 -0300

    .

---
+++ ex_1_Multiplicacao4/src/Multiplicacao.java
@@ -0,0 +1,55 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class Multiplicacao {
+    private int valor;
+    private int resultado;
+
+    public Multiplicacao() {
+        this(0, 0);
+    }
+
+    public Multiplicacao(int valor, int resultado) {
+        this.valor = valor;
+        this.resultado = resultado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+    
+    
+    public void processarMultiplicacao() {
+        valor = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do número que deseja multiplicar por 4:"));  
+    }
+   
+    public void multiplicacao4() {
+        resultado = valor * 4;
+    }
+   
+    public void mostrarMultiplicacao() {
+        JOptionPane.showMessageDialog(null, "O resultado da multiplicação por 4 é: " + resultado);
+    } 
+    
+}
