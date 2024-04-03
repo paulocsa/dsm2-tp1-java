1f3f1afde329eb5d9524d997cac3872a30cf768a
paulocsa <paulocandiani04@gmail.com>
2024-04-02 22:22:10 -0300

    .

---
+++ ProjetoFormularioBD/src/Modelagem/Cliente.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelagem;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class Cliente {
+
+    private int codigo;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Cliente() {
+        this(0,"","","");
+    }
+
+    public Cliente(int codigo, String nome, String telefone, String email) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    private void cadastrar(){
+        String sql;
+        sql = "insert into Cliente(codigo,nome,telefone,email) values";
+        
+       
+    }
+    
+    
+    
+    
+    
+    
+}
